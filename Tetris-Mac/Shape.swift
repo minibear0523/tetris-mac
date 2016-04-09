802709cef1dd15434bff320ee71faa7a8ca23b8c
minibear <lei.zhang.bupt08@gmail.com>
2016-04-09 23:43:15 +0800

    MOD:增加shape类

---
+++ Tetris-Mac/Shape.swift
@@ -0,0 +1,202 @@
+//
+//  Shape.swift
+//  Tetris-Mac
+//
+//  Created by ZhangLei on 16/4/9.
+//  Copyright © 2016年 MiniBear0523. All rights reserved.
+//
+
+import SpriteKit
+
+let NumOrientation: UInt32 = 4
+let NumShapeTypes: UInt32 = 7
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+enum Orientation: Int, CustomStringConvertible {
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String {
+        switch self {
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+        }
+    }
+    
+    static func random() -> Orientation {
+        return Orientation(rawValue: Int(arc4random_uniform(NumOrientation)))!
+    }
+    
+    static func rotate(orientation: Orientation, clockwise: Bool) -> Orientation {
+        var rotated = orientation.rawValue + (clockwise ? 1 : -1)
+        if rotated > Orientation.TwoSeventy.rawValue {
+            rotated = Orientation.Zero.rawValue
+        } else if rotated < 0 {
+            rotated = Orientation.TwoSeventy.rawValue
+        }
+        
+        return Orientation(rawValue: rotated)!
+    }
+}
+
+class Shape: Hashable, CustomStringConvertible {
+    let color: BlockColor
+    var blocks = Array<Block>()
+    var orientation: Orientation
+    var column, row: Int
+    
+    /// 方块的位置
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [:]
+    }
+    
+    /// 针对不同的方向的方块最下面的位置
+    var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [:]
+    }
+    
+    var bottomBlocks: Array<Block> {
+        guard let bottomBlocks = bottomBlocksForOrientations[orientation] else {
+            return []
+        }
+        return bottomBlocks
+    }
+    
+    var hashValue: Int {
+        return blocks.reduce(0) {
+            $0.hashValue ^ $1.hashValue
+        }
+    }
+    
+    var description: String {
+        return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
+    }
+    
+    init(column: Int, row: Int, color: BlockColor, orientation: Orientation) {
+        self.color = color
+        self.column = column
+        self.row = row
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    convenience init(column: Int, row: Int) {
+        self.init(column: column, row: row, color: BlockColor.random(), orientation: Orientation.random())
+    }
+    
+    /**
+     初始化方块
+     */
+    final func initializeBlocks() {
+        guard let blockRowColumnTranslations = blockRowColumnPositions[orientation] else {
+            return
+        }
+        
+        blocks = blockRowColumnTranslations.map {
+            (diff) -> Block in
+            return Block(column: column + diff.columnDiff, row: row + diff.rowDiff, color: color)
+        }
+    }
+    
+    /**
+     旋转方块
+     
+     - parameter orientation: 方向
+     */
+    final func rotateBlocks(orientation: Orientation) {
+        guard let blockRowColumnTranslation: Array<(columnDiff: Int, rowDiff: Int)> = blockRowColumnPositions[orientation] else {
+            return
+        }
+        
+        for (idx, diff) in blockRowColumnTranslation.enumerate() {
+            blocks[idx].column = column + diff.columnDiff
+            blocks[idx].row = row + diff.rowDiff
+        }
+    }
+    
+    /**
+     顺时针旋转方块
+     */
+    final func rotateClockwise() {
+        let newOrientation = Orientation.rotate(orientation, clockwise: true)
+        rotateBlocks(newOrientation)
+        orientation = newOrientation
+    }
+    
+    /**
+     逆时针旋转方块
+     */
+    final func rotateCounterClockwise() {
+        let newOrientation = Orientation.rotate(orientation, clockwise: false)
+        rotateBlocks(newOrientation)
+        orientation = newOrientation
+    }
+    
+    final func lowerShapeByOneRow() {
+        shiftBy(0, rows: 1)
+    }
+    
+    final func raiseShapeByOneRow() {
+        shiftBy(0, rows: -1)
+    }
+    
+    final func shiftRightByOneColumn() {
+        shiftBy(1, rows: 0)
+    }
+    
+    final func shiftLeftByOneColumn() {
+        shiftBy(-1, rows: 0)
+    }
+    
+    /**
+     变换方块的位置
+     
+     - parameter columns: 列数变化
+     - parameter rows:    行数变化
+     */
+    final func shiftBy(columns: Int, rows: Int) {
+        self.column += columns
+        self.row += rows
+        for block in blocks {
+            block.column += columns
+            block.row += rows
+        }
+    }
+    
+    final func moveTo(column: Int, row: Int) {
+        self.column = column
+        self.row = row
+        rotateBlocks(orientation)
+    }
+    
+    final class func random(startingColumn: Int, startingRow: Int) -> Shape {
+        switch Int(arc4random_uniform(NumShapeTypes)) {
+        case 0:
+            return SquareShape(column: startingColumn, row: startingRow)
+        case 1:
+            return LineShape(column: startingColumn, row: startingRow)
+        case 2:
+            return TShape(column: startingColumn, row: startingRow)
+        case 3:
+            return LShape(column:startingColumn, row:startingRow)
+        case 4:
+            return JShape(column:startingColumn, row:startingRow)
+        case 5:
+            return SShape(column:startingColumn, row:startingRow)
+        default:
+            return ZShape(column:startingColumn, row:startingRow)
+        }
+    }
+}
+
+func ==(lhs: Shape, rhs: Shape) -> Bool {
+    return lhs.row == rhs.row && lhs.column == rhs.column
+}
