e2958bf6e0166597f5645ead534095f5dcadfce7
minibear <lei.zhang.bupt08@gmail.com>
2016-04-09 22:09:24 +0800

    Initial Commit

---
+++ Tetris-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tetris-Mac
+//
+//  Created by ZhangLei on 16/4/9.
+//  Copyright © 2016年 MiniBear0523. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tetris-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tetris-Mac
+//
+//  Created by ZhangLei on 16/4/9.
+//  Copyright © 2016年 MiniBear0523. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
